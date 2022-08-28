f11819506069cd8fd5bea723b4979594a4697f13
Gustavo Basulto <gbasulto.guzman@gmail.com>
2022-08-28 00:14:15 -0400

    se carga tarea

--- 072022_ProbarPython/testing_python.py
+++ 072022_ProbarPython/testing_python.py
@@ -17,3 +17,49 @@ elif(day == 'Friday'):
     print('Woohoo, time for the weekend!')
 else:
     print('Not quite there yet.')
+
+# imprime el tipo de numero
+print(type(24))
+print(type(3.9))
+print(type(3j))
+
+# proporciona un número aleatorio entre 2 y 5
+import random
+print(random.randint(2,5)) 
+
+#cadenas
+name = "Zen"
+lastname= "Alyper"
+print("Mi nombre es", name, "y mi apellido es", lastname)
+
+
+#importante al momento de trabajar con numeros en operaciones aritmeticas
+total = 35
+user_val = 26
+total = total + user_val		# salida: TypeError
+print(total)
+total = total + int(user_val)		# el total será 61
+print(total)
+
+#concatenar texto y numeros por el metodo f (Interpolación de cadenas literal)
+first_name = "Zen"
+last_name = "Coder"
+age = 27
+print(f"Mi nombre is {first_name} {last_name} and tengo {age} años de edad.")
+
+#algunos metodos de cadena
+
+edad=17
+if(edad>18):
+    cadena = 'Mayor de edad'
+else:
+    cadena="Menor de edad"
+print("El alumno es ", cadena) #pasamos la cadena a mayuscula
+
+
+#crea una lista e imprime el largo de esa lista
+mi_lista = [1, 'Zen', 'hola']
+print(len(mi_lista))
+
+print(min(mi_lista))
+
